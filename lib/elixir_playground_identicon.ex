e21e197f678df6b7aa87b1340eaf1cd725e99ce9
Brandon Johnson <bbj1979@gmail.com>
2018-10-06 02:01:55 -0600

    Initial commit

---
+++ lib/elixir_playground_identicon.ex
@@ -0,0 +1,18 @@
+defmodule ElixirPlaygroundIdenticon do
+  @moduledoc """
+  Documentation for ElixirPlaygroundIdenticon.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirPlaygroundIdenticon.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_playground_identicon_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirPlaygroundIdenticonTest do
+  use ExUnit.Case
+  doctest ElixirPlaygroundIdenticon
+
+  test "greets the world" do
+    assert ElixirPlaygroundIdenticon.hello() == :world
+  end
+end
